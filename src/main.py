4678d5c2b00ac3bd229e2f1cd278ac3876f9ad8b
nmlakra <nikhilmarklakra@gmail.com>
2025-03-09 12:25:26 +0530

    added static -> public

--- src/main.py
+++ src/main.py
@@ -1,6 +1,47 @@
 import re
 from textnode import TextType, TextNode
 from htmlnode import ParentNode, LeafNode
+import os
+import shutil
+
+
+def remove_dir_content(dir_path):
+    if os.path.exists(dir_path):
+        dir_items = os.listdir(dir_path)
+        for item in dir_items:
+            item_path = os.path.join(dir_path, item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+            else:
+                remove_dir_content(item_path)
+                os.removedirs(item_path)
+
+        print(f"Cleand {dir_path}!")
+    else:
+        print(f"{dir_path} doesn't exist!")
+
+def copy_source_files_to_destination(src_path, dst_path):
+
+    os.makedirs(dst_path, exist_ok=True)
+
+    dir_items = os.listdir(src_path)
+    for item in dir_items:
+        item_src_path = os.path.join(src_path, item)
+        item_dst_path = os.path.join(dst_path, item)
+        if os.path.isfile(item_src_path):
+            print(f"Moving {item_src_path} -> {item_dst_path}")
+            shutil.copy2(item_src_path, item_dst_path)
+        else:
+            copy_source_files_to_destination(item_src_path, item_dst_path)
+
+def copy_static_to_public():
+    source_path = "./static"
+    destination_path = "./public"
+
+    if os.path.exists(destination_path):
+        remove_dir_content(destination_path)
+
+    copy_source_files_to_destination(source_path, destination_path)
 
 def text_to_textnodes(text):
     text_node = TextNode(text, TextType.TEXT)
@@ -144,16 +185,4 @@ def text_node_to_html_node(text_node):
 
 
 if __name__ == "__main__":
-    text_node = TextNode("This is a text node", "bold")
-
-    parent_node = ParentNode(
-        "p",
-        [
-            LeafNode("b", "Bold text"),
-            LeafNode(None, "Normal text"),
-            LeafNode("i", "italic text"),
-            LeafNode(None, "Normal text"),
-        ],
-    )
-
-    print(text_node)
+    copy_static_to_public()
